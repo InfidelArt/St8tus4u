88bfec08f0a0d50b4bd898630a11c111938401d1
Jesper Johansson <jesper.v.johansson@gmail.com>
2021-10-22 08:43:17 +0200

    Create ApplicationController.java

---
+++ src/controller/ApplicationController.java
@@ -0,0 +1,31 @@
+package controller;
+
+public class ApplicationController {
+	
+	/*
+	 * Vad behöver applikationen kunna göra?
+	 * 
+	 * En metod för att logga in
+	 * En metod för att registrera ett nytt konto
+	 * En metod för att hämta data om användaren
+	 * En metod för att hämta användarens lista av aktiviteter
+	 * En metod för att hämta data om en specifik aktivitet
+	 * En metod för att lägga till en ny aktivitet
+	 * En metod för att ta bort en aktivitet
+	 * En metod för att byta namn på en aktivitet
+	 * En metod för att logga ut
+	 * 
+	 * Att göra: Skapa ett skelett för metoderna och bestäm vad de behöver som parametrar (från front-end)
+	 * och vad de returnerar (från back-end).
+	 * När vi har detta bestämt är det lätt att dela upp arbetet.
+	 */
+	
+	
+	
+	
+	public static void main(String[] args) {
+		// Ska vi ha main i controller eller i front-end?
+
+	}
+
+}
