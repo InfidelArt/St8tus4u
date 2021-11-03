34aa533b06302fb997757c2e058240945891b8e4
Jesper Johansson <jesper.v.johansson@gmail.com>
2021-11-03 16:09:00 +0100

    Dao
    fixing useDao, preparing activityDao

--- src/dao/ActivityDao.java
+++ src/dao/ActivityDao.java
@@ -25,15 +25,17 @@ public class ActivityDao implements DaoInterface {
 		return null;
 	}
 	
-	public Object save(int userId, Object t) throws DataEntryException {
+	public Activity save(int userId, Object t) throws DataEntryException {
 		
 		PreparedStatement preparedStatement = null;
 		ResultSet resultSet = null;
 		
 		try {
 			preparedStatement = dbConnectionManager.prepareStatement(
-					"INSERT INTO activity_list ()"
+					"INSERT INTO activity_list (user_id, activity_name, start_date, start_location, avarege_speed, avarege_heart_rate, total_time, start_time) "
+					+ "VALUES (?, ?, ?, ?, ?, ?, ?, ?)"
 					);
+			// TODO continue here
 		} catch (SQLException e) {
 			e.printStackTrace();
 			throw new DataEntryException("Could not enter activity into database.");
